08f90cd32c62b9478fbca6daab7f3edeafcd9953
Robby94gtr <robertogramazio94@gmail.com>
2025-04-14 18:02:42 +0200

    [END]  Created function in people that combines fullName and hobbies

--- modules/people.js
+++ modules/people.js
@@ -5,3 +5,16 @@ const fullName = require('./names');
 // 2. Importa la funzione da hobbies.js
 
 const hobbies = require ('./hobbies');
+
+/* 3. Crea una funzione che non ha parametri. La funzione dovrebbe restituire un oggetto con due proprietà: 
+fullName ed hobbies. All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto. */
+
+function people (){
+    let objPeople = {
+        nome: fullName('roberto', 'gramazio'),
+        hobbies: hobbies('suonare', 'ascoltare musica', 'leggere'),
+    };
+    return objPeople;
+};
+
+console.log(people());
